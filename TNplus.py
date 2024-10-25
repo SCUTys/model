69ffe77f67b484d1fb69f98793b91a3c10ed47ab
SCUTys <3028827066@qq.com>
2024-10-25 21:19:32 +0800

    排队论函数修正完毕，接入工程运行顺利，但数值使用有待商榷

--- TNplus.py
+++ TNplus.py
@@ -106,7 +106,7 @@ class DispatchCenter:
             for pile in charge_station.pile.keys():
                 print("Calculating charge_time")
                 print(charge_station.t_cost[pile])
-                if charge_station.t_cost[pile][1] > 0:
+                if charge_station.t_cost[pile][1] > 0 and charge_station.t_cost[pile][0] > 0:
                     charge_time[(charge_station.id, pile)] = charge_station.t_cost[pile][0] / charge_station.t_cost[pile][1]
                 else:
                     charge_time[(charge_station.id, pile)] = 0 #待商榷
@@ -140,11 +140,14 @@ class DispatchCenter:
 
         def calculate_cs_wait_time(cs_id, cs_power):
             charge_s = self.charge_stations[cs_id]
-            cs_time = charge_s.calculate_wait_cs(charge_s.pile[cs_power],
-                                                 charge_s.capacity * charge_s.pile[cs_power] / sum(charge_s.pile.values()),
-                                                 arrive_num[cs_id, cs_power],
-                                                 charge_time[cs_id, cs_power])
-            return cs_time
+            if charge_time[cs_id, cs_power] == 0:
+                return 0
+            else:
+                cs_time = charge_s.calculate_wait_cs(charge_s.pile[cs_power],
+                                                     charge_s.capacity * charge_s.pile[cs_power] / sum(charge_s.pile.values()),
+                                                     arrive_num[cs_id, cs_power],
+                                                     1 / charge_time[cs_id, cs_power])
+                return cs_time
 
 
         def sort(var, var_cost): #按cost升序排序var中成分
@@ -768,43 +771,89 @@ class ChargeStation:
                 for index, tu in enumerate(self.queue[p]):
                     self.queue[p][index] = self.center.solve_tuple(tu, t)
 
-
     def calculate_wait_cs(self, s, k, l, m):
         # s = self.pile[power]
         # k = self.capacity * self.pile[power]/ sum(self.pile.values())
         # l = self.v_arrive / T
         # if self.t_cost[1] != 0:
-        #     m = self.t_cost[0] / self.t_cost[1]
+        #     m = self.t_cost[1] / self.t_cost[0]
         # else:
         #     m = 1
+
+        def calculate_p_0(rou, s, k, rou_s):
+            p_0 = 0
+
+            for i in range(s):
+                p_0 += math.exp(i * math.log(rou) - math.log(math.factorial(i)))
+
+            if rou_s == 1:
+                log_term = math.log(k - s + 1) + s * math.log(rou) - math.log(math.factorial(s))
+                p_0 += math.exp(log_term)
+            else:
+                term1 = s * math.log(rou) - math.log(math.factorial(s))
+                term2_1 = (k - s + 1) * math.log(rou_s)
+                if rou_s < 1:
+                    term2 = math.log(1 - math.exp(term2_1)) - math.log(1 - rou_s)
+                else:
+                    term2 = math.log(math.exp(term2_1) - 1) - math.log(rou_s - 1)
+
+                log_term = term1 + term2
+                p_0 += math.exp(log_term)
+
+            return 1 / p_0
+
+        def calculate_p_k(p_0, rou, k, s):
+            if k < s:
+                print("k < s")
+                return p_0 * (rou ** k) / math.factorial(k)
+            else:
+                # 使用对数计算避免溢出
+                log_p_k = (math.log(p_0)
+                           + k * math.log(rou)
+                           - math.log(math.factorial(s))
+                           - (k - s) * math.log(s))
+                return math.exp(log_p_k)
+
+        def calculate_L_q(p_0, rou, s, k, rou_s):
+            if rou_s == 1:
+                log_L_q = math.log(p_0) + s * math.log(rou) + math.log(k - s) + math.log(k - s + 1) - math.log(
+                    2) - math.log(math.factorial(s))
+                L_q = math.exp(log_L_q)
+            else:
+                # 使用对数计算避免溢出
+                log_part1 = math.log(p_0) + s * math.log(rou) + math.log(rou_s)
+                log_part2_1 = (k - s + 1) * math.log(rou_s) + math.log(k - s)
+                log_part2_2 = (k - s) * math.log(rou_s) + math.log(k - s + 1)
+                log_part2 = math.log(1 + math.exp(log_part2_1) - math.exp(log_part2_2))
+                log_part3 = - math.log(math.factorial(s)) - math.log(math.fabs(1 - rou_s)) * 2
+
+                log_L_q = log_part1 + log_part2 + log_part3
+                L_q = math.exp(log_L_q)
+
+            return L_q
+
         print("Before calculate")
+        print("s, k, l, m: ", end='')
         print(s, k, l, m)
         if l == 0 or m == 0:
             return 0
         else:
             rou = l / m
             rou_s = rou / s
-            p_0 = 0
-            for i in range(s):
-                p_0 += rou**i / math.factorial(i)
-            if rou_s == 1:
-                p_0 += (k - s + 1) * (rou**s) / math.factorial(s)
-            else:
-                p_0 += (rou**s) * (1 - rou_s**(k - s + 1))/ math.factorial(s) / (1 - rou_s)
-            p_0 = 1 / p_0
-            p_k = p_0 * (rou**k) / math.factorial(s) / (s ** (k - s))
-            print("During calculate")
-            print(rou, k, rou**k, math.factorial(s), s, k - s)
+            p_0 = calculate_p_0(rou, s, k, rou_s)
+            p_k = calculate_p_k(p_0, rou, k, s)
+            # print("During calculate")
+            # print("rou, rou ** k, math.factorial(s), s, k - s: ", end='')
+            # print(rou, rou ** k, math.factorial(s), s, k - s)
             l_e = l * (1 - p_k)
-            if rou_s == 1:
-                L_q = p_0 * (rou**s) * (k - s) * (k - s + 1) / 2 / math.factorial(s)
-            else:
-                L_q = p_0 * (rou**s) * rou_s * (1 - (rou_s**(k - s + 1)) - (1 - rou_s) * (k - s + 1) * (rou_s**(k - s))) / math.factorial(s) / (1 - rou_s)**2
+            L_q = calculate_L_q(p_0, rou, s, k, rou_s)
             print("After calculate")
-            print(p_0, p_k, l_e, L_q)
+            print("p_0, p_k, L_q, l_e: ", end='')
+            print(p_0, p_k, L_q, l_e)
             if l_e == 0:
                 return 0
             else:
+                print(L_q / l_e)
                 return L_q / l_e
 
 
--- TNtest.py
+++ TNtest.py
@@ -6,38 +6,111 @@ def calculate_wait_cs(s, k, l, m):
     # k = self.capacity * self.pile[power]/ sum(self.pile.values())
     # l = self.v_arrive / T
     # if self.t_cost[1] != 0:
-    #     m = self.t_cost[0] / self.t_cost[1]
+    #     m = self.t_cost[1] / self.t_cost[0]
     # else:
     #     m = 1
+
+
+    def calculate_p_0(rou, s, k, rou_s):
+        p_0 = 0
+
+        for i in range(s):
+            p_0 += math.exp(i * math.log(rou) - math.log(math.factorial(i)))
+
+        if rou_s == 1:
+            log_term = math.log(k - s + 1) + s * math.log(rou) - math.log(math.factorial(s))
+            p_0 += math.exp(log_term)
+        else:
+            term1 = s * math.log(rou) - math.log(math.factorial(s))
+            term2_1 = (k - s + 1) * math.log(rou_s)
+            if rou_s < 1:
+                term2 = math.log(1 - math.exp(term2_1)) - math.log(1 - rou_s)
+            else:
+                term2 = math.log(math.exp(term2_1) - 1) - math.log(rou_s - 1)
+
+            log_term = term1 + term2
+            p_0 += math.exp(log_term)
+
+        return 1 / p_0
+
+
+    def calculate_p_k(p_0, rou, k, s):
+        if k < s:
+            print("k < s")
+            return p_0 * (rou ** k) / math.factorial(k)
+        else:
+            # 使用对数计算避免溢出
+            log_p_k = (math.log(p_0)
+                       + k * math.log(rou)
+                       - math.log(math.factorial(s))
+                       - (k - s) * math.log(s))
+            return math.exp(log_p_k)
+
+    def calculate_L_q(p_0, rou, s, k, rou_s):
+        if rou_s == 1:
+            log_L_q = math.log(p_0) + s * math.log(rou) + math.log(k - s) + math.log(k - s + 1) - math.log(2) - math.log(math.factorial(s))
+            L_q = math.exp(log_L_q)
+        else:
+            # 使用对数计算避免溢出
+            log_part1 = math.log(p_0) + s * math.log(rou) + math.log(rou_s)
+            log_part2_1 = (k - s + 1) * math.log(rou_s) + math.log(k - s)
+            log_part2_2 = (k - s) * math.log(rou_s) + math.log(k - s + 1)
+            log_part2 = math.log(1 + math.exp(log_part2_1) - math.exp(log_part2_2))
+            log_part3 = - math.log(math.factorial(s)) - math.log(math.fabs(1 - rou_s)) * 2
+
+            log_L_q = log_part1 + log_part2 + log_part3
+            L_q = math.exp(log_L_q)
+
+        return L_q
+
     print("Before calculate")
+    print("s, k, l, m: ", end='')
     print(s, k, l, m)
     if l == 0 or m == 0:
         return 0
     else:
         rou = l / m
         rou_s = rou / s
-        p_0 = 0
-        for i in range(s):
-            p_0 += rou ** i / math.factorial(i)
-        if rou_s == 1:
-            p_0 += (k - s + 1) * (rou ** s) / math.factorial(s)
-        else:
-            p_0 += (rou ** s) * (1 - rou_s ** (k - s + 1)) / math.factorial(s) / (1 - rou_s)
-        p_0 = 1 / p_0
-        p_k = p_0 * (rou ** k) / math.factorial(s) / (s ** (k - s))
-        print("During calculate")
-        print(rou, rou ** k, math.factorial(s), s, k - s)
+        p_0 = calculate_p_0(rou, s, k, rou_s)
+        p_k = calculate_p_k(p_0, rou, k, s)
+        # print("During calculate")
+        # print("rou, rou ** k, math.factorial(s), s, k - s: ", end='')
+        # print(rou, rou ** k, math.factorial(s), s, k - s)
         l_e = l * (1 - p_k)
-        if rou_s == 1:
-            L_q = p_0 * (rou ** s) * (k - s) * (k - s + 1) / 2 / math.factorial(s)
-        else:
-            L_q = p_0 * (rou**s) * rou_s * (1 - (rou_s**(k - s + 1)) - (1 - rou_s) * (k - s + 1) * (rou_s**(k - s))) / math.factorial(s) / (1 - rou_s)**2
+        L_q = calculate_L_q(p_0, rou, s, k, rou_s)
         print("After calculate")
-        print(p_0, p_k, l_e, L_q)
+        print("p_0, p_k, L_q, l_e: ", end='')
+        print(p_0, p_k, L_q, l_e)
         if l_e == 0:
             return 0
         else:
+            print(L_q / l_e)
             return L_q / l_e
 
-
+print("result testing1")
 calculate_wait_cs(2, 5, 2, 0.5)
+print('\n')
+print("result testing2")
+calculate_wait_cs(20, 50, 20, 0.5)
+print('\n')
+print("result testing3")
+calculate_wait_cs(100, 125.0, 20.0, 1 / 13.553238105736334)
+print('\n')
+print("result testing4")
+calculate_wait_cs(100, 125.0, 40.0, 1 / 13.553238105736334)
+print('\n')
+print("result testing5")
+calculate_wait_cs(100, 125.0, 2.0, 1 / 13.553238105736334)
+print('\n')
+print("result testing6")
+calculate_wait_cs(100, 125.0, 4.0, 1 / 13.553238105736334)
+print('\n')
+print("result testing7")
+calculate_wait_cs(100, 125.0, 100.0, 1 / 13.553238105736334)
+print('\n')
+print("result testing8")
+calculate_wait_cs(100, 125.0, 120.0, 1 / 13.553238105736334)
+print('\n')
+print("result testing9")
+calculate_wait_cs(20, 50, 20, 5)
+print('\n')
